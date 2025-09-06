b83d3d320bfe2eaae15501d8ff4464a22c5c13fd
Joel Coenraadts <joel@bytelab.com>
2025-09-06 15:29:07 +1000

    wip on scim api - moved schemas to subfolder

---
+++ permitta-core/src/apis/scim2/src/schemas/__init__.py
@@ -0,0 +1,2 @@
+from .user_schema import user_schema
+from .group_schema import group_schema
--- permitta-core/src/apis/scim2/src/schemas_group_api.py
+++ permitta-core/src/apis/scim2/src/schemas_group_api.py
@@ -1,5 +1,6 @@
 from app_logger import Logger, get_logger
 from flask import Blueprint, jsonify, make_response, request, Response
+from .schemas import group_schema
 
 logger: Logger = get_logger("scim2.schemas_group_api")
 bp = Blueprint(
@@ -16,98 +17,6 @@ def get_group_schema():
 
     This endpoint returns information about the Group schema.
     """
-    group_schema = {
-        "id": "urn:ietf:params:scim:schemas:core:2.0:Group",
-        "name": "Group",
-        "description": "Group",
-        "attributes": [
-            {
-                "name": "id",
-                "type": "string",
-                "multiValued": False,
-                "description": "Unique identifier for the Group",
-                "required": True,
-                "caseExact": True,
-                "mutability": "readOnly",
-                "returned": "always",
-                "uniqueness": "server",
-            },
-            {
-                "name": "displayName",
-                "type": "string",
-                "multiValued": False,
-                "description": "A human-readable name for the Group",
-                "required": True,
-                "caseExact": False,
-                "mutability": "readWrite",
-                "returned": "always",
-                "uniqueness": "server",
-            },
-            {
-                "name": "members",
-                "type": "complex",
-                "multiValued": True,
-                "description": "A list of members of the Group",
-                "required": False,
-                "subAttributes": [
-                    {
-                        "name": "value",
-                        "type": "string",
-                        "multiValued": False,
-                        "description": "Identifier of the member of this Group",
-                        "required": True,
-                        "caseExact": False,
-                        "mutability": "readWrite",
-                        "returned": "default",
-                        "uniqueness": "none",
-                    },
-                    {
-                        "name": "type",
-                        "type": "string",
-                        "multiValued": False,
-                        "description": "A label indicating the type of resource, e.g., 'User' or 'Group'",
-                        "required": False,
-                        "caseExact": False,
-                        "canonicalValues": ["User", "Group"],
-                        "mutability": "readWrite",
-                        "returned": "default",
-                        "uniqueness": "none",
-                    },
-                    {
-                        "name": "$ref",
-                        "type": "reference",
-                        "multiValued": False,
-                        "description": "The URI of the corresponding resource",
-                        "required": False,
-                        "caseExact": False,
-                        "mutability": "readWrite",
-                        "returned": "default",
-                        "uniqueness": "none",
-                        "referenceTypes": ["User", "Group"],
-                    },
-                    {
-                        "name": "display",
-                        "type": "string",
-                        "multiValued": False,
-                        "description": "A human-readable name for the member",
-                        "required": False,
-                        "caseExact": False,
-                        "mutability": "readWrite",
-                        "returned": "default",
-                        "uniqueness": "none",
-                    },
-                ],
-                "mutability": "readWrite",
-                "returned": "default",
-                "uniqueness": "none",
-            },
-        ],
-        "meta": {
-            "resourceType": "Schema",
-            "location": "/Schemas/urn:ietf:params:scim:schemas:core:2.0:Group",
-        },
-    }
-
     response: Response = make_response(
         jsonify(group_schema),
     )
